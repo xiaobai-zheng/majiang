fc7f9c81bfd74f8274091c35a915c92c86d97cd4
qiang_glb <888888_glb@zheng.com>
2020-01-08 23:55:27 +0800

    将mybatis配置文件中的错误修改

--- src/main/java/com/bilibili/majiang/demo/DemoApplication.java
+++ src/main/java/com/bilibili/majiang/demo/DemoApplication.java
@@ -1,12 +1,8 @@
 package com.bilibili.majiang.demo;
 
-import com.github.pagehelper.PageHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @MapperScan("com.bilibili.majiang.demo.mapper")
